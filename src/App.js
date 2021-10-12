333a9e0bbd7f907ec1c5577a111430d79ab648ff
testdami555 <testdami555@gmail.com>
2021-10-12 12:19:33 -0300

    Agregado el footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
       <h1>Hello CodeSandbox de Dami</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>Hoy es un gran día para comenzar con React</p>
+      <footer>Soy el footer</footer>
     </div>
   );
 }
