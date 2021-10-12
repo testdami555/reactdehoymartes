b8292b3438e41b1870ca03fa314bf21b36eb6adb
testdami555 <testdami555@gmail.com>
2021-10-12 12:18:54 -0300

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox de Dami</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>Hoy es un gran día para comenzar con React</p>
     </div>
   );
 }
